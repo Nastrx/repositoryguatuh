757d0262a70354b823f9c44832f538ef2ac83f1c
Nastrx <mghyfari@gmail.com>
2024-09-27 10:02:23 +0700

    komit++

--- HelloBapak.java
+++ HelloBapak.java
@@ -1,7 +1,8 @@
-public class HelloWord{
+public class HelloBapak{
     public static void main(String[] args){
       System.out.println("hello");
       System.out.println("Selamat belajar Jawa");
-      
+      System.out.println("Nama saya");
+      System.out.println("ghyfari");
     }
 }
